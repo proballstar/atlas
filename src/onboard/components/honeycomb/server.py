4a829a08aa345cb1f11be427257a11689f2d7630
proballstar <rohanf6219@gmail.com>
2020-02-09 00:12:19 -0800

    data displat

--- src/onboard/components/honeycomb/server.py
+++ src/onboard/components/honeycomb/server.py
@@ -7,13 +7,14 @@ def home():
     return redirect('/homepage')
 @app.route('/homepage')
 def homepage():
-    return render_template("home.html")
-@app.route('/rocket')
-def rocket():
     c1 = 1
     c2 = 2
     c3 = 3 
-    return render_template("rocket.html", c1 = c1, c2 = c2, c3 = c3)
+    return render_template("home.html", c1 = c1, c2 = c2, c3 = c3)
+@app.route('/rocket')
+def rocket():
+ 
+    return render_template("rocket.html")
 @app.route('/support')
 def support():
     return render_template("support.html")
