99d04dd71a984668f499e905be6e98e349abea10
Aaron Ma <aaronhma@gmail.com>
2020-02-11 19:23:02 -0800

    UPDATE honeycomb

--- src/onboard/components/honeycomb/medical.py
+++ src/onboard/components/honeycomb/medical.py
@@ -1,4 +1,10 @@
 import skyforce
+import tensorflow as tf
 #Add the functions to be able to use measure and collect data 
 def load_data(url):
     return url
+
+def create_model():
+    model = tf.keras.Sequential([
+        tf.keras.Dense(1)
+    ])
