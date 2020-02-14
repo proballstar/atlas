d425c419f44553743e43297ce7323de31ea9aca8
Aaron Ma <aaronhma@gmail.com>
2020-02-14 04:27:28 +0000

    PTurtle.py edited

--- shared/PTurtle.py
+++ shared/PTurtle.py
@@ -1,41 +1,41 @@
-import turtle
+from turtle import *
 import skyforce
     
 
 def init():
-    rohan = turtle.Pen()
+    hi = turtle.Pen()
 def goto(x,y):
-    rohan.penup()
-    rohan.goto(x,y)
-    rohan.pendown()
+    hi.penup()
+    hi.goto(x,y)
+    hi.pendown()
 def square(s):
     for i in range(4):
-        rohan.forward(s)
-        rohan.right(90)
+        hi.forward(s)
+        hi.right(90)
 def rectangle(l,w):
     for i in range(2):
-        rohan.forward(l)
-        rohan.right(90)
-        rohan.forward(w)
-        rohan.right(90)
+        hi.forward(l)
+        hi.right(90)
+        hi.forward(w)
+        hi.right(90)
 def circle(r):
-    rohan.circle(r)
+    hi.circle(r)
 def write(w):
-    rohan.write(w)
+    hi.write(w)
 def shape(sides,size):
     for i in range(sides):
-        rohan.forward(size)
-        rohan.right(360/sides)
+        hi.forward(size)
+        hi.right(360/sides)
 def part_circle(r,d):
-    rohan.circle(r,d)
+    hi.circle(r,d)
 def left(degree):
-    rohan.left(degree)
+    hi.left(degree)
 def right(degree):
-    rohan.right(degree)
+    hi.right(degree)
 def diag(d):
-    rohan.setheading(d)
+    hi.setheading(d)
 def up(l):
-    rohan.forward(l)
+    hi.forward(l)
 def r():
     left(90)
     up(10)
@@ -50,6 +50,7 @@ def f():
     right(90)
     up(5)
     goto(7,7)
-    
-    
-rohan = turtle.Pen()
+
+if __name__ == "__main__":
+    init()
+    # @TODO: update
