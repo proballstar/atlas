948ba6a34defa358d1ac2803da7f6092c5af95aa
Rohan Fernandes <rohanf6219@gmail.com>
2020-03-01 03:17:38 +0000

    ADDING NOTES and confirmation to PTurtle

--- shared/PTurtle.py
+++ shared/PTurtle.py
@@ -1,5 +1,8 @@
-from turtle import *
-import skyforce
+#PROJECT PTURTLE -- ROHAN FERNANDES -- TURTLE FUNCTIONS ONLY
+#@NOTE: ONLY USE IF HAVE CONSENT FROM ROHAN FERNANDES 
+#V 0.0.1 PTURTLE -- MODIFY VERSION FOR PROJECT ATLAS: FAST, FUEL EFFICIENT, THE OFFICAL SPACE ROCKET OF THE FIREBOLT SPACE AGENCY (FSA)
+from turtle import * #getting all functions from Turtle
+import skyforce #import skyforce that is in shared folder
     
 
 def init():
@@ -8,6 +11,8 @@ def goto(x,y):
     hi.penup()
     hi.goto(x,y)
     hi.pendown()
+def goto_line(x,y):
+    hi.goto(x,y)
 def square(s):
     for i in range(4):
         hi.forward(s)
@@ -51,7 +56,5 @@ def f():
     up(5)
     goto(7,7)
 
-if __name__ == "__main__":
-    init()
-    # @TODO: update
-    # @TODO: Aaron how do update 
+    # @NOTE: This file is only for turtle functions, if wanted copy into directory and import into file to use for GUI Purpose, or use for using the function IN SEPERATE FILE. THIS FILE IS ONLY MEANT FOR 
+    # @NOTE: TURTLE FUNCTION DEFINITIONS NOTHING ELSE
