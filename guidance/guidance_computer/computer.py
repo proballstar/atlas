877d4b8965bf0b7301277a9e887d222598a72cb2
Aaron Ma <aaronhma@gmail.com>
2020-03-11 14:31:49 -0700

    UPDATE computer.py

--- guidance/guidance_computer/computer.py
+++ guidance/guidance_computer/computer.py
@@ -1,7 +1,7 @@
 from . import *
 
 class Computer(object):
-    def __init__(self, map_file='~/Desktop/atlas/maps/moon.map'):
+    def __init__(self, map_file='~/Desktop/atlas/guidance/guidance_computer/moon.map'):
         self.map = map_file
     
     def input(self):
