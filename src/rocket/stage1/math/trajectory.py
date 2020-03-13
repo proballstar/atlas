b424afcc7a6bdbd96ebf8902d186edbab7d2c3d7
proballstar <rohanf6219@gmail.com>
2020-03-12 23:27:17 -0700

    small adds

--- src/rocket/stage1/math/trajectory.py
+++ src/rocket/stage1/math/trajectory.py
@@ -1,17 +1,17 @@
 # @TODO(aaronhma): Create trajectory variables
 TRAJECTORY = (0x60)
-DESTINATION = "" # choose from moon, mars, etc.
-TRAJECTORY_X = 
-TRAJECTORY_Y = 
-TRAJECTORY_Z = 
+DESTINATION = "Moom" # choose from moon, mars, etc.
+TRAJECTORY_X = "" # @TODO: Update with Trajectory Stats for Moon Landing
+TRAJECTORY_Y = ""
+TRAJECTORY_Z = ""
 
 # @TODO(aaronhma): put all the trajectory and destinations into file: trajectory.txt
 
 # @TODO(aaronhma): destroy all useless variables with ```del``` to save RAM
 del TRAJECTORY
 del DESTINATION
-del ____
-del ____
-del ____
+del TRAJECTORY_X
+del TRAJECTORY_Y
+del TRAJECTORY_Z
 
 # @TODO(aaronhma): add a formal ending
