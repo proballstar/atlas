ce9a6592bd453d3329cc38f6c5d4b983e4c1fb75
Aaron Ma <aaronhma@gmail.com>
2020-03-15 09:37:41 -0700

    UPDATE stage2

--- src/rocket/stage2/src/rocket_engines/fire_engines.ino
+++ src/rocket/stage2/src/rocket_engines/fire_engines.ino
@@ -13,11 +13,11 @@ void setup() {
 }
 
 void loop() {
-    System.sleep(SLEEP_MODE_CPU);
-    if (toggle == true) {
+    System.sleep(SLEEP_MODE_CPU); // Put system on Low Power Mode
+    if (toggle == true) {         // Transistor Gate ON
         digitalWrite(firePin, HIGH);
-        delay(4000);
-        digitalWrite(firePin, LOW);
+        delay(4000);              // Pin active for 4 seconds
+        digitalWrite(firePin, LOW); // Turn off transistor gate
         toggle = false;
     }
 }
