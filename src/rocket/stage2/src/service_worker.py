584444839093ba55b746841fe6ebfc519adc65f4
Rohan Fernandes <rohanf6219@gmail.com>
2020-03-16 21:46:20 +0000

    service_worker.py edited online with Bitbucket

--- src/rocket/stage2/src/service_worker.py
+++ src/rocket/stage2/src/service_worker.py
@@ -6,7 +6,7 @@ import pandas as pd
 # complete - @TODO(aaronhma, rohan): Step 1. Load service_worker.json
 service_worker = pd.read_json('./service_worker.json') # load the json file
 
-service_workerx = service_worker.loc[service_worker.short_name == f'atlas_stage2'] # select only stage2 data
+service_workerx = service_worker.loc[service_worker.short_name == f'atlas_stage2'] # select only stage2 data (#@I thought it was iloc not loc 
 print(service_workerx) # print filtered data
 print("\n\ns") # sep
 print(service_worker.head()) # show all data
