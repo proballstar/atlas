4676f58c27c469ef4ebcd6970adeacdb10559953
Aaron Ma <aaronhma@gmail.com>
2020-03-16 14:45:05 -0700

    UPDATE service_worker in stage2

--- src/rocket/stage2/src/service_worker.py
+++ src/rocket/stage2/src/service_worker.py
@@ -3,10 +3,12 @@ Service worker to run offline.
 """
 import pandas as pd
 
-# @TODO(aaronhma, rohan): Step 1. Load service_worker.json
-service_worker = pd.read_json('./service_worker.json')
-# @TODO(aaronhma, rohan): Step 2. Make stage2 offline.
-service_workerx = service_worker.loc[service_worker.short_name == f'atlas_stage2']
-print(service_workerx)
-print("\n\ns")
-print(service_worker.head())
+# complete - @TODO(aaronhma, rohan): Step 1. Load service_worker.json
+service_worker = pd.read_json('./service_worker.json') # load the json file
+
+service_workerx = service_worker.loc[service_worker.short_name == f'atlas_stage2'] # select only stage2 data
+print(service_workerx) # print filtered data
+print("\n\ns") # sep
+print(service_worker.head()) # show all data
+
+# todo - @TODO(aaronhma, rohan): Make stage 2 offline
