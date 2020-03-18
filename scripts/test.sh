c33fda40cc5d3957d214b294ad05d8ff4c61f62a
Rohan Fernandes <rohanf6219@gmail.com>
2020-03-18 01:42:33 +0000

    update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,8 @@
 # @TODO(aaronhma, rohan): UPDATE
+
+atlas_root = "~atlas"
+honeycomb = atlas_root + "src/onboard/components/honeycomb"
+rocket  = atlas_root + "src/rocket"
+stage_1 = rocket + "stage1/"
+stage_2 = rocket + "stage2/"
+stage_3 = rocket + "stage3/"
