cac89551a5a2a5817047ac44845d8c1cad801d7a
Aaron Ma <aaronhma@gmail.com>
2020-03-27 20:23:35 -0700

    UPDATE: shared

--- shared/get_local_time.cc
+++ shared/get_local_time.cc
@@ -1,3 +1,8 @@
+// Copyright 2020 - Present Aaron Ma.
+// All Rights Reserved.
+// This file is part of the Atlas Project.
+// You may not use this file without agreeing
+// to the Atlas license.
 #include <iostream>
 #include <string>
 using namespace std;
