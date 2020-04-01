c27bc8baecb3d1da8339cc97275ee74e2ca1df8a
aaronhma <aaronhma@gmail.com>
2020-03-31 20:21:16 -0700

    Update atlas.sh

--- scripts/atlas.sh
+++ scripts/atlas.sh
@@ -1,3 +1,12 @@
-ATLAS_ROOT = "~Desktop\Firebolt\atlas"
-#!For Rohan:~"C:\Users\Rohan Fernandes\python_bit\aaron\progects\atlas"
-#!For Aaron: "~Desktop\Firebolt\atlas
+#!/bin/bash
+ATLAS_ROOT = "/atlas/"
+
+function help() {
+  cat <<EOF
+  To run atlas.sh, please specify parameters.
+EOF
+}
+
+function print_sep() {
+  echo '================'
+}
