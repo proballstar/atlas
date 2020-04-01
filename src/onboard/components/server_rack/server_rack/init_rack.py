dae63802741b5ccdd4ef4f9cc986ac0f12c07292
aaronhma <aaronhma@gmail.com>
2020-04-01 09:15:51 -0700

    Update init_rack.py

--- src/onboard/components/server_rack/server_rack/init_rack.py
+++ src/onboard/components/server_rack/server_rack/init_rack.py
@@ -1,3 +1,20 @@
-import sys
-sys.path.append('../../../../../')
-print("Initializing server rack...")
+class Comanche:
+  def __init__(self, name = 'Comanche'):
+    self.name = name
+
+class Luminary:
+  def __init__(self, name = 'Luminary'):
+    self.name = name
+ 
+sep = "============================"
+server_rack_init = "Initializing server rack: {}"
+welcome = "Welcome to Server Rack Initializer!"
+init_server_before = "Initializing server rack..."
+print(sep)
+print(welcome)
+# Start by initializing the main server rack, Comanche
+comanche = Comanche()
+print(server_rack_init.format(comanche.name))
+print(sep)
+luminary = Luminary()
+print(server_rack_init.format(luminary.name))
