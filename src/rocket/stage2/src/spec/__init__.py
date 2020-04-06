f07659e501065bd1bd549b7bf96ddc48ab2f4ba3
aaronhma <aaronhma@gmail.com>
2020-04-05 20:37:52 -0700

    Update __init__.py

--- src/rocket/stage2/src/spec/__init__.py
+++ src/rocket/stage2/src/spec/__init__.py
@@ -1,1 +1,12 @@
-from . import hardware_test as bit_test
+from . import radiation_test as test_radiation
+
+dev_mode = False
+
+if __name__ == "__main__":
+  try:
+    test_radiation.test()
+  except AssertionError as err:
+    if dev_mode != False:
+      raise AssertionErrror(err)
+    else:
+      raise AssertionError("The flight computer has radiation in it. Please reboot it and contact Mission Control.")
