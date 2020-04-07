62fe8967bbd7450588b58c8abf12d73d94e30f73
aaronhma <aaronhma@gmail.com>
2020-04-07 14:11:20 -0700

    Update test_camera.py

--- src/rocket/stage2/spec/test_camera.py
+++ src/rocket/stage2/spec/test_camera.py
@@ -2,7 +2,7 @@ try:
     from src import camera
 
 except ImportError:
-    print("Couldn't load Camera!")
+    raise ImportError("Couldn't load Camera!")
     
 def test_camera(mirror_status=True):
     camera.camera(mirror_status=mirror_status)
